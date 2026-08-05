cf8e49e713d377e171dc2d90fd5a27f61abc8807
k90918 <k90918@users.noreply.github.com>
2026-08-05 22:29:18 +0800

    feat: 新增 GWL 小額終身壽險試算

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'premium-calculator-v1';
+const CACHE = 'premium-calculator-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
